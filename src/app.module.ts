5598dd0f1571581991a73c5bcfbb553c024f2250
garchomp-game <otoka.japanesedrum@gmail.com>
2024-07-31 12:12:54 +0900

    cats delete

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsModule } from './cats/cats.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [CatsModule, AuthModule, UserModule],
+  imports: [AuthModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
